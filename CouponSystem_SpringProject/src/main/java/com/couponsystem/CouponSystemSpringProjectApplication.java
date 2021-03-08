3a05ec76a553d296bb3ee72a2196b6f3075068e1
hllyl <hlly.lov@gmail.com>
2021-03-08 09:38:54 +0200

    ...

---
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.couponsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CouponSystemSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CouponSystemSpringProjectApplication.class, args);
+	}
+
+}
