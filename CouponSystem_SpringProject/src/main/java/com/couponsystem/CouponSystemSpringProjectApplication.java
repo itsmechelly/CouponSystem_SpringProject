22681376b6812bbfb59191bc88ad88d1cf3cc516
hllyl <hlly.lov@gmail.com>
2021-03-08 09:41:13 +0200

    ...

--- CouponSystem_SpringProject/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//TODO -> @EnableScheduling
 public class CouponSystemSpringProjectApplication {
 
 	public static void main(String[] args) {
