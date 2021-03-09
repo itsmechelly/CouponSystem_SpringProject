c0a817e28a3edfaeed25b4944f015db30b283cc0
hllyl <hlly.lov@gmail.com>
2021-03-09 13:25:59 +0200

    managed exceptions

--- CouponSystem_SpringProject/src/main/java/com/couponsystem/exceptions/NotFoundException.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/exceptions/NotFoundException.java
@@ -1,5 +1,17 @@
 package com.couponsystem.exceptions;
 
-public class NotFoundException {
+public class NotFoundException extends CouponSystemException {
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public NotFoundException(String s) {
+		super("System error, unable to find " + s);
+	}
+
+	public NotFoundException(String s, double d) {
+		super("System error, unable to find " + s + d + ".");
+	}
 }
