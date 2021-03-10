079a44c791f702198e0f9dec6bed961ded402bfd
hllyl <hlly.lov@gmail.com>
2021-03-10 11:50:54 +0200

    Login functionality

--- CouponSystem_SpringProject/src/main/java/com/couponsystem/security/LoginManagerService.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/security/LoginManagerService.java
@@ -1,5 +1,65 @@
 package com.couponsystem.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Lazy;
+import org.springframework.stereotype.Service;
+
+import com.couponsystem.enums.ClientType;
+import com.couponsystem.exceptions.CouponSystemException;
+import com.couponsystem.exceptions.LogException;
+import com.couponsystem.service.AdminService;
+import com.couponsystem.service.CompanyService;
+import com.couponsystem.service.CustomerService;
+
+@Service
+@Lazy
 public class LoginManagerService {
+
+	private ApplicationContext ctx;
+	private TokenManager tokenManager;
+	private final AdminService adminService;
+	private CompanyService companyService;
+	private CustomerService customerService;
+	
+	@Autowired
+	public LoginManagerService(ApplicationContext ctx, TokenManager tokenManager, AdminService adminService,
+			CompanyService companyService, CustomerService customerService) {
+		super();
+		this.ctx = ctx;
+		this.tokenManager = tokenManager;
+		this.adminService = adminService;
+		this.companyService = companyService;
+		this.customerService = customerService;
+	}
+	
+	public String login(String email, String password, ClientType clientType) throws CouponSystemException {
+
+		switch (clientType) {
+		
+		case ADMIN:
+			if (adminService.login(email, password)) {
+				return tokenManager.addToken(adminService);
+			}
+			
+		case COMPANY:
+			companyService = ctx.getBean(CompanyService.class);
+			if (companyService.login(email, password)) {
+				int companyId = companyService.findIdByEmailAndPassword(email, password);
+				companyService.setCompanyId(companyId);
+				return tokenManager.addToken(companyService);
+			}
+			
+		case CUSTOMER:
+			customerService = ctx.getBean(CustomerService.class);
+			if (customerService.login(email, password)) {
+				int customerId = customerService.findIdByEmailAndPassword(email, password);
+				customerService.setCustomerId(customerId);
+				return tokenManager.addToken(customerService);
+			}
+		default:
+			throw new LogException();
+		}
+	}
 
 }
--- CouponSystem_SpringProject/src/main/java/com/couponsystem/security/TokenManager.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/security/TokenManager.java
@@ -1,5 +1,31 @@
 package com.couponsystem.security;
 
+import java.sql.Date;
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import com.couponsystem.service.ClientService;
+
+@Component
 public class TokenManager {
 
+	private String token;
+	private HashMap<String, CustomSession> tokensInMemory;
+
+	@Autowired
+	public TokenManager(String token, HashMap<String, CustomSession> tokensInMemory) {
+		super();
+		this.token = token;
+		this.tokensInMemory = tokensInMemory;
+	}
+
+	public String addToken(ClientService clientService) {
+		this.token = UUID.randomUUID().toString();
+		tokensInMemory.put(token, new CustomSession(clientService, new Date(Instant.now().toEpochMilli())));
+		return token;
+	}
 }
