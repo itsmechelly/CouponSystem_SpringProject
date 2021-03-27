c841286ba96fea242724ac58c6205b16c19c749c
Chelly izraelov <hlly.lov@gmail.com>
2021-03-27 15:08:22 +0300

    Dbdao package was deleted permanent

--- CouponSystem_SpringProject/src/main/java/com/couponsystem/service/ClientService.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/service/ClientService.java
@@ -3,9 +3,9 @@ package com.couponsystem.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.couponsystem.dbdao.CompanyDbdao;
-import com.couponsystem.dbdao.CouponDbdao;
-import com.couponsystem.dbdao.CustomerDbdao;
+//import com.couponsystem.dbdao.CompanyDbdao;
+//import com.couponsystem.dbdao.CouponDbdao;
+//import com.couponsystem.dbdao.CustomerDbdao;
 import com.couponsystem.exceptions.CouponSystemException;
 
 @Service
