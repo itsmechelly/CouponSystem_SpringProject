87f6858ff32066d9729eb8efc14fb13912b7a8f7
Chelly izraelov <hlly.lov@gmail.com>
2021-04-15 21:10:49 +0300

    New login management

---
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/security/Session.java
@@ -0,0 +1,71 @@
+package com.couponsystem.security;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import com.couponsystem.enums.ClientType;
+
+@Component
+@Scope("prototype")
+public class Session {
+
+	// fields
+	public final String token; // identifier of a session
+	private Map<ClientType, Object> attributes = new HashMap<>(); // session state
+	private long lastAccessed; // needed for managing session expiration
+	@Value("${session.max.inactive.interval:2}") // injection happens after CTOR
+	private long maxInactiveInterval; // in milliseconds - needed for managing session expiration
+	private static final int TOKEN_MAX_LENGTH = 15;
+	
+	// Initialiser (runs before CTOR) - first step after object creation
+	{
+		this.token = UUID.randomUUID().toString().replace("-", "").substring(0, TOKEN_MAX_LENGTH);
+		resetLastAccessed();
+	}
+
+	@PostConstruct // runs after CTOR
+	private void init() {
+		// fix the time units - for convenience
+		maxInactiveInterval = TimeUnit.MINUTES.toMillis(maxInactiveInterval);
+	}
+
+	// reset the time of the session each time the client was active
+	public void resetLastAccessed() {
+		this.lastAccessed = System.currentTimeMillis();
+	}
+
+	// this is needed for session context to decide on session invalidation
+	public long getLastAccessed() {
+		return lastAccessed;
+	}
+
+	public void setAttribute(ClientType clientType, Object attrVal) {
+		attributes.put(clientType, attrVal);
+	}
+
+	public Object getAttribute(ClientType clientType) {
+		return attributes.get(clientType);
+	}
+
+	// this is needed for session context to decide on session invalidation
+	public long getMaxInactiveInterval() {
+		return maxInactiveInterval;
+	}
+
+	/**
+	 * @param maxInactiveInterval given in mills
+	 */
+	//TODO - DELETE? DO YOU USE IT? IF SO, SET THE TIME WITH THE RELEVENT METHOD YOU HAVE ABOVE. (:
+	public void setMaxInactiveInterval(long maxInactiveInterval) {
+		this.maxInactiveInterval = maxInactiveInterval;
+	}
+
+}
