5c42ac63bcdbf5fe6f4b5b5c3b602829a19b2af8
Chelly izraelov <hlly.lov@gmail.com>
2021-04-21 11:41:42 +0300

    Removed old login method from CompanyController

--- CouponSystem_SpringProject/src/main/java/com/couponsystem/rest/CompanyController.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/rest/CompanyController.java
@@ -1,7 +1,6 @@
 package com.couponsystem.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -17,64 +16,28 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.couponsystem.beans.Coupon;
-import com.couponsystem.beans.LoginResponse;
 import com.couponsystem.enums.ClientType;
 import com.couponsystem.enums.CouponCategory;
 import com.couponsystem.exceptions.AlreadyExistException;
 import com.couponsystem.exceptions.LogException;
 import com.couponsystem.exceptions.NotAllowedException;
 import com.couponsystem.exceptions.NotFoundException;
-import com.couponsystem.security.LoginManagerService;
-import com.couponsystem.security.TokenManager;
 import com.couponsystem.service.CompanyService;
 import com.couponsystem.security.SessionContext;
 
 @RestController
 @RequestMapping("/company")
 @CrossOrigin(origins = "http://localhost:4200", allowedHeaders = "*")
-public class CompanyController {//extends ClientController {
+public class CompanyController {
 
-	private final LoginManagerService loginManagerService;
 	private final SessionContext sessionContext;
 
 	@Autowired
-	public CompanyController(LoginManagerService loginManagerService,
-			TokenManager tokenManager, SessionContext sessionContext) {
+	public CompanyController(SessionContext sessionContext) {
 		super();
-		this.loginManagerService = loginManagerService;
 		this.sessionContext = sessionContext;
 	}
-
-//	------------------------------------------------------------------------------------------------------------
-
-//	@PostMapping("/login")
-//	@Override
-//	public ResponseEntity<?> ClientLogin(String email, String password) throws LogException {
-//
-//		HttpHeaders responseHeaders = new HttpHeaders();
-//
-//		try {
-//			String token = loginManagerService.login(email, password, ClientType.COMPANY);
-//			responseHeaders.set("CouponSystem_Header", token);
-//			System.out.println("Company token = " + token);
-//			
-//			LoginResponse loginResponse = new LoginResponse();
-//			loginResponse.setToken(token);
-//			loginResponse.setType("company");
-//			return ResponseEntity.ok().headers(responseHeaders).body(loginResponse);
-//
-//		} catch (LogException e) {
-//			return new ResponseEntity<>(e.getMessage(), HttpStatus.UNAUTHORIZED);
-//		}
-//
-//	}
-//
-//	@Override
-//	public ResponseEntity<?> ClientLogout() throws LogException {
-//		// TODO Auto-generated method stub
-//		return null;
-//	}
-
+	
 //	------------------------------------------------------------------------------------------------------------
 
 	@PostMapping("/addCompanyCoupon")
