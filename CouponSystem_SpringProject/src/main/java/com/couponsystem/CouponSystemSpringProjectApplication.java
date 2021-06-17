59340724f4e7a4473945995371a2ad86512a8f99
Chelly izraelov <hlly.lov@gmail.com>
2021-06-17 14:16:22 +0300

    Project version with JWT technique is starting

--- CouponSystem_SpringProject/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -9,7 +9,6 @@ public class CouponSystemSpringProjectApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CouponSystemSpringProjectApplication.class, args);
-		System.out.println("The git version up to here is with Session Context technique. from this point, I will change all to JWT technique");
 	}
 
 }
