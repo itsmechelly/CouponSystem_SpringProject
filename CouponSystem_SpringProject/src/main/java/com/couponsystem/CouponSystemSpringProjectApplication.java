1b774ba49a5bea819901b9b77deb12e24f4334af
Chelly izraelov <hlly.lov@gmail.com>
2023-02-28 10:36:42 +0200

    ...

--- CouponSystem_SpringProject/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
+++ CouponSystem_SpringProject/src/main/java/com/couponsystem/CouponSystemSpringProjectApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CouponSystemSpringProjectApplication {
 
 	public static void main(String[] args) {
+		sss
 		SpringApplication.run(CouponSystemSpringProjectApplication.class, args);
 	}
 
